928e07c4bffab0c09836b1ba5260a9ab4190f3e1
GABETROLL <gabriel7icplhappy@gmail.com>
2022-11-30 17:28:24 +0000

    Fixed beggining space crashing shell.

--- main.c
+++ main.c
@@ -9,19 +9,39 @@ int main(void)
 {
 	int status = 1;
 
+	/* to get line from stdin: */
+	char *stdin_line = NULL;
+	char **line_tokens = NULL;
+
 	while (status == 1)
 	{
-		char **line_tokens;
+		size_t buffsize = 100;
+
+		stdin_line = NULL;
+		line_tokens = NULL;
 
 		printf("$ ");
 
-		line_tokens = stdin_tokens();
+		getline(&stdin_line, &buffsize, stdin);
+		if (!stdin_line)
+			continue;
+
+		line_tokens = str_tokens(stdin_line);
+		if (!line_tokens)
+		{
+			free(stdin_line);
+			continue;
+		}
 
 		status = create_fork(line_tokens);
 
-		free(*line_tokens);
 		free(line_tokens);
+		free(stdin_line);
 	}
+
+	free(line_tokens);
+	free(stdin_line);
+
 	return (0);
 }
 
