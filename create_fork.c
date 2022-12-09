8cb9d6289583f8d803ec62f80854173e02e2c881
GABETROLL <gabriel7icplhappy@gmail.com>
2022-12-09 14:10:00 +0000

    Fix more betty

--- create_fork.c
+++ create_fork.c
@@ -1,5 +1,16 @@
 #include "main.h"
-
+/**
+ * create_fork - If this process is the parent process,
+ * it copies itself and switches to running the executable
+ * in 'tokens', just like in the Linux shell, but without
+ * special character functionality.
+ *
+ * If the executale name doesn't exist, just return 0.
+ *
+ * If this process is the child, it waits for the parent
+ * process to finish, and then continues normally
+ * by returning 1.
+ */
 int create_fork(char *shell_name, char **tokens, char **env, int is_terminal)
 {
 	pid_t child_pid;
@@ -18,33 +29,23 @@ int create_fork(char *shell_name, char **tokens, char **env, int is_terminal)
 
 		if (!tokens || !tokens[0])
 			return (0);
-
 		execve(tokens[0], tokens, env);
-
-		/*
-		 * Everything past the above line is
-		 * due to an executable not being found.
-		 *
-		 * if the stdin wasn't empty:
-		 */
-
+		/* this only runs if execve running 'tokens[0]' failed: */
 		path = get_path(env);
 		full_file_name = split_path(tokens[0], path);
-
 		if (full_file_name != NULL && path != NULL)
 		{
 			char *exec_name = tokens[0];
 
 			tokens[0] = full_file_name;
 			execve(tokens[0], tokens, env);
+			/* this only runs if execve running 'full_path_name' failed: */
 			tokens[0] = exec_name;
 			free(full_file_name);
 		}
-
 		if (tokens != NULL && *tokens != NULL)
 		{
 			write(1, shell_name, _strlen(shell_name));
-
 			if (is_terminal)
 				write(1, ": No such file or directory\n", 28);
 			else
@@ -54,13 +55,10 @@ int create_fork(char *shell_name, char **tokens, char **env, int is_terminal)
 				write(1, ": not found\n", 12);
 			}
 		}
-
 		return (0);
 	}
 	else
-	{
 		wait(&status);
-	}
 	return (1);
 }
 
